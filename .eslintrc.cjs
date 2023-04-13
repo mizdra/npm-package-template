5ce97347ef4c8b9d44f8f81ad8e22e8159dae332
mizdra <pp.mizdra@gmail.com>
2023-04-13 20:37:23 +0900

    eslint config 微調整

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,10 +2,21 @@
 module.exports = {
   root: true,
   extends: ['@mizdra/mizdra', '@mizdra/mizdra/+typescript', '@mizdra/mizdra/+prettier'],
+  parserOptions: {
+    ecmaVersion: 2022,
+  },
   env: {
     node: true,
   },
   rules: {
     'import/no-unresolved': 'off',
   },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx', '*.cts', '*.mts'],
+      parserOptions: {
+        project: ['./tsconfig.json'],
+      },
+    },
+  ],
 };
