30e38e6fae9f69a5fbcd3c86c5803e5d212ef10e
mizdra <pp.mizdra@gmail.com>
2024-08-16 13:11:22 +0900

    組み込みの GitHub Actions Reporter を使う

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vitest/config';
-import GithubActionsReporter from 'vitest-github-actions-reporter';
 
 // eslint-disable-next-line import/no-default-export
 export default defineConfig({
   test: {
-    reporters: process.env['GITHUB_ACTIONS'] ? ['default', new GithubActionsReporter()] : 'default',
+    reporters: process.env['GITHUB_ACTIONS'] ? ['default', 'github-actions'] : 'default',
     cache: {
       dir: 'node_modules/.cache/vitest',
     },
