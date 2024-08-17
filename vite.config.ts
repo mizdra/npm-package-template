ca1c430f2199065ddf1b538fe0a0f861fcf7735d
mizdra <pp.mizdra@gmail.com>
2024-08-18 01:25:38 +0900

    flat config に切り替える

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vitest/config';
 
-// eslint-disable-next-line import/no-default-export
+// eslint-disable-next-line import-x/no-default-export
 export default defineConfig({
   test: {
     reporters: process.env['GITHUB_ACTIONS'] ? ['default', 'github-actions'] : 'default',
